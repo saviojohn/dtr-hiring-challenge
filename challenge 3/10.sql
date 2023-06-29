bb2c01c17220f44d1cddc8fb747174e056cafba8
savio <savio.john.t@gmail.com>
2023-06-29 13:22:52 +0530

    challenge 3 completed

---
+++ challenge 3/10.sql
@@ -0,0 +1,3 @@
+alter table users
+add active boolean default true;
+
---
+++ challenge 3/8.sql
@@ -0,0 +1,11 @@
+select max(sum_column) as max_sum , subquery.business_id, subquery.business_name
+from ( 
+  select sum(amount) as sum_column , business.business_id, business.business_name
+  from sales
+  inner join business
+  on business.business_id = sales.business_id
+  where sales.created_time >= '2021-01-01' and sales.created_time <= '2021-01-31'
+  group by business.business_id
+  order by sum_column desc
+) as subquery
+group by subquery.business_id, subquery.business_name;
